d77597434d147df43e53aaa5901ee426f468b35a
kdin <dineshkannan99@gmail.com>
2015-02-17 18:21:43 +0530

    Scratchpad-Version 2.0

--- scratchpad_v2/assets/js/dependencies/application.js
+++ scratchpad_v2/assets/js/dependencies/application.js
@@ -1,5 +1,17 @@
+/**
+* Defines module-scratchpadModule for the application
+*@dependencies: ui-router , Angular - resource 
+*@states: 2 Parent states and 1 Child state
+*@Controllers: Controllers for listing, viewing and adding a scratch
+*@Factory Service: Notes for the angular resource
+*
+*
+*
+*
+*/
 var scratchpadModule = angular.module('scratchpad',['ui.router','ngResource']);
 
+/*----------Scratchpad Configuration States---------------*/
 scratchpadModule.config(function($stateProvider,$httpProvider) {
 	
 	$stateProvider.state('scratchpad',{
@@ -17,35 +29,53 @@ scratchpadModule.config(function($stateProvider,$httpProvider) {
 		templateUrl:'templates/viewScratch.html',
 		controller:'viewScratchController'
 	})
-	.state('addNewScratch.emailScratch',{
-		url:'/emailScratch',
-		templateUrl:'templates/emailScratch.html',
-		controller:'addScratchController'
-	})
-	;
 }).run(function($state){
 	$state.go('scratchpad');
 });
 
+/*----------Scratchpad Configuration States---------------*/
 
+/*----------Scratchpad Controllers---------------*/
 
 scratchpadModule.controller('scratchListController', function($scope,$state,Notes){
 	
 	$scope.scratchpad = Notes.query();
 
-	$scope.deleteScratch = function (scratch) {
-		if(confirm("You Clicked DELETE !! Are you sure ?")) {
-			scratch.$delete(function() {
+	$scope.deleteScratch = function (scratch,flag) {
+		if(flag === 0) {											//Checks if Bulk delete or single delete
+			if(confirm("You Clicked DELETE !! Are you sure ?")) {
+			scratch.$delete(function() {							//Single delete
 				window.location.href = 'http://localhost:1337';
 			});
+			}	
 		}
+		else {														//Bulk delete
+			scratch.$delete(function() {
+				window.location.href = 'http://localhost:1337';
+			});		
+		}
+		
 	}
+
+	$scope.emptyScratchpad = function () {
+		var ask = false;
+		if (confirm ("You are about Empty Scratchpad. Sure ????")) {
+			ask = true;	
+		}
+		if(ask === true) {
+			for (var i = 0; i < $scope.scratchpad.length; i++) {
+				$scope.deleteScratch($scope.scratchpad[i],1);
+			}
+		}
+		
+	}	
 })
 
 .controller('viewScratchController', function($scope,$stateParams,Notes){
 	
-	$scope.scratch = Notes.get({id:$stateParams.id});
+	$scope.scratch = Notes.get({id:$stateParams.id});			//get scratch with id in state paremeters
 })
+
 
 .controller('addScratchController', function($scope,$state,Notes){
 	
@@ -56,33 +86,38 @@ scratchpadModule.controller('scratchListController', function($scope,$state,Note
 		$scope.scratch.$save(function () {
 			$state.go('scratchpad');
 		});
+
 	}
 	$scope.emailScratch = function() {
+			console.log($scope.scratch.mailid);
+			console.log($scope.scratch.title);
+			console.log($scope.scratch.content);
+		/*--------------------------------Email using mandrill----------------------*/
 
-				/*Email using mandrill*/
+		//#####################   API Call to Mandrill API   ###################
 			$.ajax({
       		type: 'POST',
 		      url: 'https://mandrillapp.com/api/1.0/messages/send.json',
 		      data: {
-		        'key': 'u1IAL_R9ZDaboyElRz9TMQ',
+		        'key': 'u1IAL_R9ZDaboyElRz9TMQ',	//API key -> should come from process file
 		        'message': {
 		          'from_email': 'YOURSCRATCH@scratchpad.com',
 		          'to': [
 		              {
-		                'email': 'anshumanv@visualbi.com',
+		                'email': $scope.scratch.mailid,	// Receiver's email id
 		                'name': 'Dinesh Kannan',
 		                'type': 'to'
 		              }
 		            ],
 		          'autotext': 'false',
-		          'subject': $scope.scratch.title,
-		          'html': $scope.scratch.content		       
+		          'subject': $scope.scratch.title,		//Scratch Title as the subject of the email
+		          'html': $scope.scratch.content		//Scratch Content as the body of the email       
 		           }
 		      }
-		     }).done(function(response) {
-		       console.log(response); 
+		     }).done(function(response) {				//Response of the email logged
+		       console.log(response); 					
 		     });
-				/*Email using mandrill*/
+	/*----------------------------Email using mandrill----------------------*/
 				$scope.addScratch();
 		
 	
@@ -91,16 +126,14 @@ scratchpadModule.controller('scratchListController', function($scope,$state,Note
 })
 ;
 
+/*----------Scratchpad Controllers---------------*/
 
-
-
-
-
+/*----------Notes factory for the resource---------------*/
 scratchpadModule.factory('Notes', function($resource){
 	return $resource('http://localhost:1337/scratchpadAPI/:id', {id:'@id'}, {
-
 		update: {
 			method:'PUT'
 		}
 	});
 });
+/*----------Notes factory for the resource---------------*/
