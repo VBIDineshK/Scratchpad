4498291fabb6b55fc954ede6b40b8b5832c3a692
kdin <dineshkannan99@gmail.com>
2015-02-17 18:27:45 +0530

    Dependencies

---
+++ scratchpad_v2/assets/js/dependencies/bower_components/sails-mandrill/README.md
@@ -0,0 +1,151 @@
+sails-mandrill
+==============
+
+Mandrill adapter for sails.js
+
+You can use this adapter directly, or from a model.  I recommend you set up a connection, link it to a model, and access the functionality that way.  It makes for cleaner and more reusable code.
+For instructions on using the adapter directly, see the bottom of this file.  
+
+
+## Create a connection to your Mandrill account
+
+> Note: these instructions are for sails v0.10.x (currently unreleased, but available as the #associations branch on github).
+> You'll probably be able to use this adapter with sails@v0.9.x as well, but you'll need to use the `adapters` config, in `config/adapters.js`, and set the `adapter:'mandrill'` key in your model (instead of `connections`).
+
+###### config/connections.js
+```javascript
+module.exports.connections = {
+
+  // ...
+  
+  // Mandrill
+  'my favorite mandrill account': {
+    adapter: 'sails-mandrill',
+    apiKey: process.env.MANDRILL_KEY, // the api key for your mandrill account
+    from: {
+      name: 'Default sender to use ( can be overriden in options to .send() )',
+      email: 'default@sender.com'
+    }
+  }
+};
+
+```
+
+
+## Hook up a Model to your new connection
+
+###### config/models/Email.js
+```javascript
+module.exports = {
+  connections: ['my favorite mandrill account']
+};
+
+```
+
+\
+
+## Usage
+
+#### `Model.send ( [options], [callback] )`
+
+Generally, `sails-mandrill` accepts the same inputs as the official mandrill SDK for Node.js.  It does, however, simplify the usage a bit for the most basic options.
+You can override any of your connection defaults (including your API key!!!) in the options.
+
+
+
+###### Send an HTML email:
+```javascript
+
+Email.send({
+  to: [{
+    name: 'Alvin',
+    email: 'alvin@chipmunks.com'
+  }, {
+    name: 'Chipettes',
+    email: 'team@chipettes.com'
+  }],
+  subject: 'omg i love you guys!!1',
+  html: 
+    'I can\'t wait to see you all in Chicago<br/>' +
+    'I loe you so much!!!! ',
+  text: 'text fallback goes here-- in case some recipients (let\'s say the Chipettes)  can\'t receive HTML emails'
+}, function optionalCallback (err) {
+  // If you need to wait to find out if the email was sent successfully,
+  // or run some code once you know one way or the other, here's where you can do that.
+  // If `err` is set, the send failed.  Otherwise, we're good!
+});
+
+
+// A lot of times, in your controller, you may not necessarily want to wait until the email sends
+// In that case, you can go ahead and send your response down here.
+// Just keep in mind that delivery of an email is never 100% guaranteed, no matter what SMTP cloud you're using!
+
+
+```
+
+
+###### Send a template from your Mandrill dashboard:
+TODO: document this
+
+
+#### `Model.listTemplates ( [options], [callback] )`
+
+Get your templates. The **key** property is required.
+
+```javascript
+Email.listTemplates({
+  key: API_KEY,
+  label: "example-label"
+}, function optionalCallback (err, templates) {
+    console.log(templates);
+});
+```
+
+#### `Model.addTemplate ( [options], [callback] )`
+
+Add a template. The **key** and **name** properties are required
+
+```javascript
+Email.listTemplates({
+  key: API_KEY,
+  name: "Example Template",
+  from_email: "from_email@example.com",
+  from_name: "Example Name",
+  subject: "example subject",
+  code: "<h1>example code</h1>",
+  text: "Example text content",
+  publish: false,
+  labels: [
+    "example-label"
+  ]
+}, function optionalCallback (err, template) {
+    console.log(template);
+});
+```
+
+
+
+
+
+
+## Using the adapter directly
+
+As mentioned above, you can use the adapter directly.  It's pretty much the same, except you won't have any default connection config mixed into your options, so you'll have to include your API key and `from` config (ie. sender) each time you send an email:
+
+```javascript
+var sendEmail = function (options, cb) {
+  // Pass null as the first argument, since you aren't using the adapter from the context of a model.
+  // (normally, the model name is passed implicitly as the first argument)
+  sails.adapters.mandrill.send(null, options, cb);
+};
+
+sendEmail({
+  /* all the same stuff, just make sure you include everyting 
+   * you'd include in your connections configuration as well
+   */
+}, function optionalCallback (err) {
+  // If you need to wait to find out if the email was sent successfully,
+  // or run some code once you know one way or the other, here's where you can do that.
+  // If `err` is set, the send failed.  Otherwise, we're good!
+});
+```
---
+++ scratchpad_v2/assets/js/dependencies/bower_components/sails-mandrill/index.js
@@ -0,0 +1,338 @@
+/**
+ * Adapter dependencies
+ */
+
+var Mandrill = require('mandrill-api').Mandrill,
+	_ = require('lodash'),
+	request = require('request');
+_.defaults = require('merge-defaults');
+
+
+module.exports = (function() {
+
+	// Connection configurations
+	var _connections = {};
+
+	// Base url for API requests.
+	var BASE_URL = 'https://mandrillapp.com/api/1.0';
+
+	var Adapter = {
+
+
+
+		// App-wide defaults for this adapter
+		defaults: {
+			from: {
+				name: '<Your App>',
+				email: 'contact@yourapp.com'
+			}
+		},
+
+
+
+		registerConnection: function(connection, collections, cb) {
+
+			// Absorb adapter defaults into connection config
+			var connectionConf = _.defaults(_.cloneDeep(connection), Adapter.defaults);
+
+			// Store each collection config for later
+			_connections[connection.identity] = {
+				config: connectionConf,
+				collections: collections
+			};
+
+			cb();
+		},
+
+
+		teardown: function(connID, cb) {
+
+			cb();
+		},
+
+
+
+		/**
+		 * Send an email
+		 *
+		 * @param {String} connID - [optional]
+		 *    Connection identity or null
+		 *
+		 * @param {String} collID - [optional]
+		 *		Collection/Model identity or null
+		 *
+		 * @param {Object} options - [optional]
+		 *		TODO: Waterline core should automatically apply model config to options
+		 *
+		 * @param {Function} cb - [optional]
+		 */
+
+		send: function(connID, collID, options, cb) {
+
+			cb = _.isFunction(cb) ? cb :
+				_.isFunction(options) ? options :
+				function() {};
+			options = _extendOptions(connID, options);
+
+			var err = _validateOptions(options);
+			if (err) return cb(err);
+
+			// Set-up API key
+			var mandrill = new Mandrill(options.apiKey);
+			delete options.apiKey;
+
+			// Tolerate rowdy inputs
+			options = _marshalOptions(options);
+
+			// Handle `sendTemplate`
+			if (options.template) {
+
+				// Validate and marshal template
+				if (!options.template) return cb('`template` must be specified');
+				options.data = options.data || {};
+
+				// Map everything to Mandrill's expectations
+				// and send the message
+				mandrill.messages.sendTemplate(
+					_serializeTemplateOptions(options),
+					function success(result) {
+						cb(null, result);
+					},
+					function error(err) {
+						cb(err);
+					});
+				return;
+			}
+
+			// Map everything to Mandrill's expectations
+			// and send the message
+			mandrill.messages.send(
+				_serializeOptions(options),
+				function success(result) {
+					cb(null, result);
+				},
+				function error(err) {
+					cb(err);
+				});
+		},
+
+
+
+		/*******************
+		 * Template Methods
+		 *******************/
+
+		/**
+		 * Get all mandrill templates.
+		 *
+		 * @param  {String}   connID     [Connection identity or null]
+		 * @param  {String}   collID     [Collection/Model identity or null]
+		 * @param  {Object}   options [Options used in request to find templates]
+		 * @param  {Function} cb      [Callback that passes errors or templates]
+		 */
+		listTemplates: function(connID, collID, options, cb) {
+			cb = _.isFunction(cb) ? cb :
+				_.isFunction(options) ? options :
+				function() {};
+			options = _extendOptions(collID, options);
+
+			request.post({
+				url: BASE_URL + '/templates/list.json',
+				form: {
+					key: options.apiKey
+				}
+			}, function(err, response, templates) {
+				if (err) return cb(err);
+				return cb(null, templates);
+			});
+		},
+
+		/**
+		 * [addTemplate description]
+		 *
+		 * @param  {String}   connID     [Connection identity or null]
+		 * @param  {String}   collID     [Collection/Model identity or null]
+		 * @param  {Object}   options [Options used in request to add template]
+		 * @param  {Function} cb      [Callback that passes errors or added template]
+		 */
+		addTemplate: function(connID, collID, options, cb) {
+			cb = _.isFunction(cb) ? cb :
+				_.isFunction(options) ? options :
+				function() {};
+			options = _extendOptions(collID, options);
+			request.post({
+				url: BASE_URL + '/templates/add.json',
+				form: {
+					key: options.apiKey,
+					name: options.name,
+					from_email: options.from_email,
+					from_name: options.from_name,
+					subject: options.subject,
+					code: options.code,
+					text: options.text || undefined,
+					publish: options.publish,
+					labels: options.labels
+				}
+			}, function(err, response, template) {
+				if (err) return cb(err);
+				return cb(null, template);
+			});
+		}
+
+
+	};
+
+	return Adapter;
+
+
+
+	/**
+	 * Extend usage options with collection configuration
+	 * (which also includes adapter defaults)
+	 * @api private
+	 */
+	function _extendOptions(connID, options) {
+
+		// Ignore unexpected options, use {} instead
+		options = _.isPlainObject(options) ? options : {};
+
+		// Apply connection defaults, if relevant
+		if (connID) {
+			return _.extend({}, _connections[connID].config, options);
+		}
+		return _.extend({}, options);
+	}
+
+
+
+	/**
+	 * @returns truthy value if options are invalid or incomplete
+	 * @api private
+	 */
+	function _validateOptions(options) {
+
+		if (!options.to) {
+			return '`to` must be specified, e.g.: { to: { email: "foo@bar.com", name: "Mr. Foo Bar" } }';
+		}
+		if (!options.from) {
+			return '`from` must be specified, e.g.: { from: { email: "foo@bar.com", name: "Mr. Foo Bar" } }';
+		}
+	}
+
+
+
+	/**
+	 * @returns {Object} options with types normalized for consistency
+	 * @api private
+	 */
+	function _marshalOptions(options) {
+
+		if (_.isString(options.to)) {
+			options.to = [{
+				email: options.to
+			}];
+		}
+		if (_.isPlainObject(options.to)) {
+			options.to = [options.to];
+		}
+		_.map(options.to, function(recipient) {
+			if (_.isString(recipient)) {
+				return {
+					email: recipient
+				};
+			}
+			return recipient;
+		});
+
+		if (_.isString(options.from)) {
+			options.from = {
+				email: options.from
+			};
+		}
+		return options;
+	}
+
+
+
+	/**
+	 * @returns {Object} options formatted for use w/ mandrill
+	 * (`send`)
+	 * @api private
+	 */
+	function _serializeOptions(options) {
+
+		var serialized = _.merge({}, options, {
+			message: {
+				to: options.to,
+				subject: options.subject,
+				html: options.html || '',
+				text: options.text || undefined,
+				from_email: options.from.email,
+				from_name: options.from.name
+			}
+		});
+		delete serialized.to;
+		delete serialized.subject;
+		delete serialized.from;
+		delete serialized.html;
+		delete serialized.text;
+		return serialized;
+	}
+
+
+
+	/**
+	 * @returns {Object} options formatted for use w/ mandrill
+	 * (`sendTemplate`)
+	 * @api private
+	 */
+	function _serializeTemplateOptions(options) {
+
+		// Build transformed data array
+		var mappedData = [];
+		_.each(options.data, function(v, k) {
+			recursiveTransform(null, v, k);
+		});
+
+		// TODO: limit max depth and use tail-recursion
+		function recursiveTransform(keyPrefix, value, key) {
+
+			// Recursive case
+
+			// Handle Waterline models
+			if (_.isObject(value) && value.toJSON) {
+				value = value.toJSON();
+			}
+			// (break complex objects into pieces)
+			if (_.isPlainObject(value)) {
+				_.each(value, function(v, k) {
+					recursiveTransform(key + '_', v, k);
+				});
+				return;
+			}
+
+			// Base case
+			mappedData.push({
+				name: keyPrefix ? keyPrefix + key : key,
+				content: value
+			});
+		}
+
+		var serialized = _.merge({}, options, {
+			template_name: options.template,
+			template_content: mappedData,
+			message: {
+				global_merge_vars: mappedData,
+				to: options.to,
+				from_email: options.from.email,
+				from_name: options.from.name
+			}
+		});
+		delete serialized.template;
+		delete serialized.data;
+		delete serialized.to;
+		delete serialized.from;
+		return serialized;
+	}
+
+})();
